046fc8b4be0700313af75e9c2362da3b4c8b78f9
Einsler Lee <shenxiaogll@163.com>
2018-09-11 22:31:59 +0800

    main

---
+++ Leet1/src/main/main.java
@@ -0,0 +1,15 @@
+package main;
+
+import Leet.*;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String test="-2147483647";
+		q8 solution=new q8();
+		int res=solution.myAtoi(test);
+		System.out.println(res);
+	}
+
+}
