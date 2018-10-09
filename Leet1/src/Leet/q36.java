d68a8e85c9d7c51fed22fb61526de2183575b4eb
Einsler Lee <shenxiaogll@163.com>
2018-10-09 15:52:32 +0800

    Q36，42.5%。判断给定数独是否合法，有点烦，需要注意num3的序号，没用HashMap。

--- Leet1/src/Leet/q36.java
+++ Leet1/src/Leet/q36.java
@@ -1,5 +1,41 @@
 package Leet;
 
 public class q36 {
-
+	public boolean isValidSudoku(char[][] board) {
+		for(int i=0;i<9;i++) {
+			int[] rows=new int[9],cols=new int[9],boxes=new int[9];	
+			for(int j=0;j<9;j++) {
+				int num1=Integer.valueOf(board[i][j])-Integer.valueOf('1');
+				int num2=Integer.valueOf(board[j][i])-Integer.valueOf('1');
+				int num3=Integer.valueOf(board[i/3*3+j/3][(i%3)*3+j%3])-Integer.valueOf('1');
+				if(num1>=0&&num1<=8){
+                    rows[num1]++;
+                }else if(num1!=-3){
+                    return false;
+                }
+                if(num2>=0&&num2<=8){
+                    cols[num2]++;
+                }else if(num2!=-3){
+                    return false;
+                }
+                if(num3>=0&&num3<=8){
+                    boxes[num3]++;
+                }else if(num3!=-3){
+                    return false;
+                }
+			}
+			if(!isvalid(rows)||!isvalid(cols)||!isvalid(boxes)) {
+				return false;
+			}
+		}
+		return true;
+    }
+	boolean isvalid(int[] nums) {
+		for (int i=0;i<nums.length;i++) {
+			if(nums[i]>1) {
+				return false;
+			}
+		}
+		return true;
+	}
 }
--- Leet1/src/main/main.java
+++ Leet1/src/main/main.java
@@ -9,12 +9,9 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		q34 solution=new q34();
+		q36 solution=new q36();
 		//------------------------------------------
-		int[] nums= {1,2,3,3,3,3,4,5,9};
-		int[] res=solution.searchRange(nums,3);
-		for(int eachres:res) {
-			System.out.println(eachres);
-		}
+		char[][] test= {{'.','.','.','.','.','.','.','.','.'},{'.','.','.','.','.','.','.','.','.'},{'.','9','.','.','.','.','.','.','1'},{'8','.','.','.','.','.','.','.','.'},{'.','9','9','3','5','7','.','.','.'},{'.','.','.','.','.','.','.','4','.'},{'.','.','.','8','.','.','.','.','.'},{'.','1','.','.','.','.','4','.','9'},{'.','.','.','5','.','4','.','.','.'}};
+		System.out.println(solution.isValidSudoku(test));
 	}
 }
