00544e80c23a87ccd03a2073b463b3fd294329aa
Einsler Lee <shenxiaogll@163.com>
2018-11-03 23:31:05 +0800

    Q75,利用平衡的方式对原有数组根据大小排列的重建。

--- Leet1/src/Leet/q75.java
+++ Leet1/src/Leet/q75.java
@@ -2,17 +2,17 @@ package Leet;
 
 public class q75 {
 	public void sortColors(int[] nums) {
-		int start0=-1,start1=-1,start2=-1;
+		int end0=-1,end1=-1,end2=-1;
 		for(int i=0;i<nums.length;i++) {
 			if(nums[i]==0) {
-				nums[++start2]=2;
-				nums[++start1]=1;
-				nums[++start0]=0;
+				nums[++end2]=2;
+				nums[++end1]=1;
+				nums[++end0]=0;
 			}else if(nums[i]==1) {
-				nums[++start2]=2;
-				nums[++start1]=1;
+				nums[++end2]=2;
+				nums[++end1]=1;
 			}else {
-				nums[++start2]=2;
+				nums[++end2]=2;
 			}
 		}
     }
