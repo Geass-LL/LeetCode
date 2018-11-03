90e6392f67600d5f644fec076a6639d3532f226a
Einsler Lee <shenxiaogll@163.com>
2018-11-03 23:31:58 +0800

    Q74，普通问题，可化成一行做二分查找，可先行后列二分查找，竟然第一种效率更高。

--- Leet1/src/Leet/q74.java
+++ Leet1/src/Leet/q74.java
@@ -1,5 +1,41 @@
 package Leet;
 
+/*
+ * 先查行，再查列，mad好像并没有直接化成一行查的快。
+ */
 public class q74 {
-
+	public boolean searchMatrix(int[][] matrix, int target) {
+		if(matrix.length==0||matrix==null)
+			return false;
+		int m=matrix.length,n=matrix[0].length;
+		if(n==0)
+			return false;
+		int left=0,right=m-1,mid=0,sel;
+		while(left<=right) {
+			mid=(left+right)/2;
+			if(matrix[mid][0]>target)
+				right=mid-1;
+			else if(matrix[mid][0]<target)
+				left=mid+1;
+			else
+				return true;
+		}
+        if(matrix[mid][0]>target)
+            if(mid>0)   sel=mid-1;
+            else return false;
+        else
+            sel=mid;
+		left=0;
+		right=n-1;
+		while(left<=right) {
+			mid=(left+right)/2;
+			if(matrix[sel][mid]<target)
+				left=mid+1;
+			else if(matrix[sel][mid]>target)
+				right=mid-1;
+			else
+				return true;
+		}
+		return false;
+    }
 }
