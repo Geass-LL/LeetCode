a5e642a2b26898215f378212796ecde6de23bde7
Einsler Lee <shenxiaogll@163.com>
2018-11-04 12:33:44 +0800

    Q80，Two pointers,不懂为什么自己的运行时间这么长？感觉差不多呀。。

---
+++ Leet1/src/Leet/q80.java
@@ -0,0 +1,29 @@
+package Leet;
+
+/*
+ * two pointers£¬±È½Ï¼òµ¥¡£
+ */
+public class q80 {
+    public int removeDuplicates(int[] nums) {
+    	int length=nums.length;
+    	if(length<=2) {
+    		return length;
+    	}
+    	int left=1,count=0;
+    	for(int right=1;right<length;right++) {
+    		if(nums[right]!=nums[right-1]) {
+    			nums[left]=nums[right];
+    			left++;
+    			count=0;
+    		}
+    		else {
+    			if(count<1) {
+    				count++;
+    				nums[left]=nums[right];
+    				left++;
+    			}
+    		}
+    	}
+    	return left;
+    }
+}
