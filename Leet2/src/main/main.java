9d8acc1dbabd0918e2fd5ba97dab7005bb906871
Einsler Lee <shenxiaogll@163.com>
2018-11-09 18:38:12 +0800

    init

--- Leet2/src/main/main.java
+++ Leet2/src/main/main.java
@@ -11,10 +11,7 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		q99 solution=new q99();
-		TreeNode root=new TreeNode(1);
-		root.left=new TreeNode(2);
-		root.right=new TreeNode(3);
-		root.Show();
+		q101 solution=new q101();
+		
 	}
 }
