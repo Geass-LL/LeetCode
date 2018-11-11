2345ab858749e021bef8bbd93bd6659fc292a346
Einsler Lee <shenxiaogll@163.com>
2018-11-11 11:57:46 +0800

    Q101，判断二叉树是否对称。

--- Leet1/src/DS/TreeNode.java
+++ Leet1/src/DS/TreeNode.java
@@ -1,8 +1,25 @@
 package DS;
 
+import java.util.*;
+
 public class TreeNode {
 	public int val;
 	public TreeNode left;
 	public TreeNode right;
 	public TreeNode(int x){val=x;}
+	public void Show() {
+		List<Integer> showtemp=new ArrayList();
+		traverse(showtemp,this);
+		for(int eachint:showtemp) {
+			System.out.print(eachint);
+			System.out.print(" ");
+		}
+	}
+	public void traverse(List<Integer> res,TreeNode root) {
+		if(root!=null) {
+			traverse(res,root.left);
+			res.add(root.val);
+			traverse(res,root.right);
+		}
+	}
 }
--- Leet2/src/Leet/q101.java
+++ Leet2/src/Leet/q101.java
@@ -1,5 +1,23 @@
 package Leet;
 
-public class q101 {
+import DS.TreeNode;
 
+/*
+ * 挺有意思的，需要再看看。
+ */
+
+public class q101 {
+	public boolean isSymmetric(TreeNode root) {
+		if(root==null) return true;
+		else return isSymmetric(root.left,root.right);
+    }
+	boolean isSymmetric(TreeNode left,TreeNode right) {
+		if(left==null&&right==null) {
+			return true;
+		}else if(left!=null&&right!=null) {
+			return isSymmetric(left.left,right.right)&&isSymmetric(left.right,right.left)&&(left.val==right.val);
+		}else {
+			return false;
+		}
+	}
 }
