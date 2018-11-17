63f40842086ac4a27b40b6196c3e16c5cbeaeaa0
Einsler Lee <shenxiaogll@163.com>
2018-11-17 22:11:27 +0800

    Q114，递归的思路，注释的写法请背过。

--- Leet2/src/Leet/q114.java
+++ Leet2/src/Leet/q114.java
@@ -2,7 +2,17 @@ package Leet;
 
 import DS.TreeNode;
 
-
+/*
+ * private TreeNode prev = null;
+    public void flatten(TreeNode root) {
+        if (root == null) return;
+        flatten(root.right);
+        flatten(root.left);
+        root.right = prev;
+        root.left = null;
+        prev = root;
+    }
+ */
 
 public class q114 {
 	public void flatten(TreeNode root) {
