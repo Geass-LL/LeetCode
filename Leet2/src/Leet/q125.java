d4f68e30166edab63c7652a729afcd0528fdb109
Einsler Lee <shenxiaogll@163.com>
2018-11-19 23:10:42 +0800

    Q125，Easy すぎ。

---
+++ Leet2/src/Leet/q125.java
@@ -0,0 +1,22 @@
+package Leet;
+
+public class q125 {
+	public boolean isPalindrome(String s) {
+		char[] cha = s.toCharArray();
+        int i = 0, j = cha.length - 1;
+        while(i < j){
+            if(!Character.isLetterOrDigit(cha[i]))
+                i++;
+            else if(!Character.isLetterOrDigit(cha[j]))
+                j--;
+            else
+                if(Character.toLowerCase(cha[i]) == Character.toLowerCase(cha[j])){
+                    i++;
+                    j--;
+                }else{
+                    return false;
+                }
+        }
+        return true;
+    }
+}
