8128ff53a4a4987baf8c8ea36983a96ccaff6940
Einsler Lee <shenxiaogll@163.com>
2019-04-07 19:17:00 +0800

    Q201，100%。想不到的思路，还是博客上写得好！

--- Leet3/src/Leet/q201.java
+++ Leet3/src/Leet/q201.java
@@ -1,7 +1,20 @@
 package Leet;
 
+/*
+ * 就是看m和n左边的相同部分；
+ * 一直右移，直到左边相同；
+ * 将此时的值左移相同位数返回。
+ */
+
+
 public class q201 {
     public int rangeBitwiseAnd(int m, int n) {
-        return 0;
+        int i=0;
+        while(m!=n) {
+        	m>>=1;
+        	n>>=1;
+        	i++;
+        }
+        return m<<i;
     }
 }
