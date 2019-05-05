ce295421abcf69f16a34c776a10a9643cfd88a4b
Einsler Lee <shenxiaogll@163.com>
2019-05-05 21:50:25 +0800

    Q224，自己不会，需要复习！！！

--- Leet3/src/Leet/q224.java
+++ Leet3/src/Leet/q224.java
@@ -6,8 +6,11 @@ public class q224 {
 	public int calculate(String s) {
         /*
         思路：使用res记录当前结果，sign记录当前符号，利用栈来实现括号的优先计算
-        遍历字符串中的字符，如果是数字那么继续往后遍历直到不是数字为止获取整个数，res+=sign*num；如果是+，那么sign为1；如果是-，那么sign为-1；如果是左括号
-        那么将当前res、sign的值依次压入栈中，然后将res重置为0、sign重置为1准备计算括号中的值；如果是右括号，说明res中当前存的是括号中计算的结果，那么弹出栈顶的sign，res*=sign，继续弹出栈顶存储的之前的res，res+=res
+        遍历字符串中的字符，如果是数字那么继续往后遍历直到不是数字为止获取整个数，res+=sign*num；
+        如果是+，那么sign为1；如果是-，那么sign为-1；
+        如果是左括号那么将当前res、sign的值依次压入栈中，然后将res重置为0、sign重置为1准备计算括号中的值；
+        如果是右括号，说明res中当前存的是括号中计算的结果，那么弹出栈顶的sign，res*=sign，
+        继续弹出栈顶存储的之前的res，res+=res
          */
         int res=0;
         int sign=1;
