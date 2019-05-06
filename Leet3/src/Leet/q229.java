5f3bcbe9705c6e67eb33fffe64320afc6dcff2ad
Einsler Lee <shenxiaogll@163.com>
2019-05-06 22:54:31 +0800

    Q229，100%。参考博客，摩尔投票法。

---
+++ Leet3/src/Leet/q229.java
@@ -0,0 +1,35 @@
+package Leet;
+
+import java.util.*;
+
+public class q229 {
+	public List<Integer> majorityElement(int[] nums) {
+		List<Integer> res=new ArrayList<Integer>();
+		int num1=0,num2=0,count1=0,count2=0;
+		for(int i=0;i<nums.length;i++) {
+			if(nums[i]==num1) {
+				count1++;
+			}else if(nums[i]==num2) {
+				count2++;
+			}else if(count1==0) {
+				num1=nums[i];
+				count1=1;
+			}else if(count2==0) {
+				num2=nums[i];
+				count2=1;
+			}else {
+				count1--;
+				count2--;
+			}
+		}
+		count1=0;
+		count2=0;
+		for(int i=0;i<nums.length;i++) {
+			if(nums[i]==num1)	count1++;
+			else if(nums[i]==num2) count2++;
+		}
+		if(count1>nums.length/3)	res.add(num1);
+		if(count2>nums.length/3)    res.add(num2);
+		return res;
+    }
+}
