7a723d9bf99c4706743991b4d9e68c987ef810ea
Einsler Lee <shenxiaogll@163.com>
2019-05-09 21:34:40 +0800

    Q235，100%。根据二叉树的特点去查找，直到路径不同。

--- Leet3/src/Leet/q235.java
+++ Leet3/src/Leet/q235.java
@@ -10,35 +10,14 @@ import DS.TreeNode;
 
 public class q235 {
 	public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-		if(root==null||p==null||q==null)	return null;
-		List<TreeNode> resp=new ArrayList<TreeNode>(),resq=new ArrayList<TreeNode>();
-		resq.add(root);
-		resp.add(root);
-		int[] state= {0};
-		btk(root,p,resp,state);
-		state[0]=0;
-		btk(root,q,resq,state);
-		int i=0;
-		for(i=0;i<Math.min(resq.size(), resp.size());i++) {
-			if(resq.get(i)!=resp.get(i))	break;
+		if(root==null||p==null||q==null) {
+			return null;
 		}
-		return resq.get(i-1);
-    }
-	void btk(TreeNode root,TreeNode target,List<TreeNode> res,int[] state) {
-		if(root==target) {
-			state[0]=1;
-			return;
-		}else {
-			if(root.left!=null) {
-				res.add(root.left);
-				btk(root.left,target,res,state);
-				if(state[0]==0)	res.remove(res.size()-1);
-			}
-			if(root.right!=null) {
-				res.add(root.right);
-				btk(root.right,target,res,state);
-				if(state[0]==0)	res.remove(res.size()-1);
-			}
+		while(root!=null) {
+			if(p.val<root.val&&q.val<root.val)	root=root.left;
+			else if(p.val>root.val&&q.val>root.val)	root=root.right;
+			else	break;
 		}
+		return root;
 	}
 }
