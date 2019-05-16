fdd13ae9acef824074f657981795a17a85d709fe
Einsler Lee <shenxiaogll@163.com>
2019-05-16 21:28:12 +0800

    Q287，0%！！！我没想到！！！智商压制！！！二分法还行吧，快慢指针真的是不行了。

---
+++ Leet3/src/Leet/q287.java
@@ -0,0 +1,33 @@
+package Leet;
+
+public class q287 {
+	/*二分法，一步步缩小范围
+	public int findDuplicate(int[] nums) {
+        int left = 0, right = nums.length;
+        while (left < right){
+            int mid = left + (right - left) / 2, cnt = 0;
+            for (int num : nums) {
+                if (num <= mid) ++cnt;
+            }
+            if (cnt <= mid) left = mid + 1;
+            else right = mid;
+        }    
+        return right;
+    }*/
+	//竟然会用快慢指针来解，智商压制。。
+	public int findDuplicate(int[] nums) {
+        int slow = nums[0];
+        int fast = nums[0];
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+        int ptr1 = nums[0];
+        int ptr2 = slow;
+        while (ptr1 != ptr2) {
+            ptr1 = nums[ptr1];
+            ptr2 = nums[ptr2];
+        }
+        return ptr1;
+    }
+}
